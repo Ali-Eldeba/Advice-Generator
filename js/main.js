24191bcbe11deb007bd70762d5400852b503ed96
Ali-Eldeba <88302640+Ali-Eldeba@users.noreply.github.com>
2022-04-18 14:12:45 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 async function generate() {
   const res = await fetch("https://api.adviceslip.com/advice");
   const data = await res.json();
-  document.querySelector("p").innerText = "A D V I S E # " + data.slip.id;
+  document.querySelector("h1").innerText = "A D V I S E # " + data.slip.id;
   document.querySelector("q").innerText = data.slip.advice;
 }
 document.querySelector("button").onclick = () => {
